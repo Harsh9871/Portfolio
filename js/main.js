b310563e9ff9f05965c2aed2b75bd2bbec38059a
Harsh Raithatha <158036399+Harsh9871@users.noreply.github.com>
2024-05-15 11:34:44 +0530

    half website is ready

--- js/main.js
+++ js/main.js
@@ -91,15 +91,16 @@
 
 	/*--/ Star Typed /--*/
 	if ($('.text-slider').length == 1) {
-    var typed_strings = $('.text-slider-items').text();
+		var typed_strings = [ 'Backend Developer', 'Educator','Web Developer', 'Mentor','Web Designer'];
 		var typed = new Typed('.text-slider', {
-			strings: typed_strings.split(','),
-			typeSpeed: 80,
+			strings: typed_strings,
+			typeSpeed: 80, 
 			loop: true,
 			backDelay: 1100,
 			backSpeed: 30
 		});
 	}
+	
 
 	/*--/ Testimonials owl /--*/
 	$('#testimonial-mf').owlCarousel({
